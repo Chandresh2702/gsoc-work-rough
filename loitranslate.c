698625086c64c46333ad692762cf15ef29f2e430
chandresh2702 <chandresh2702@gmail.com>
2022-07-18 11:44:11 +0530

    starting loitranslate.c

--- loitranslate.c
+++ loitranslate.c
@@ -1,1 +1,14 @@
 #include <liblouis.h>
+
+int lou_translate(
+  const char *tableList,
+  const widechar *inbuf,
+  int *inlen,
+  widechar *outbuf,
+  int *outlen,
+  formtype *typeform,
+  char *spacing,
+  int *outputPos,
+  int *inputPos,
+  int *cursorPos,
+  int mode);
